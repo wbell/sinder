a557ac88b4b8cc53e58c5e68f52dd9a0ab4ad34c
Willie Bell <willie.bell@pwc.com>
2016-10-10 11:40:02 -0400

    comment settings factory

--- src/components/settings/factories/settings.factory.js
+++ src/components/settings/factories/settings.factory.js
@@ -2,15 +2,21 @@
 
 var settingsFactory = function settingsFactory($rootScope, $state, $log, $firebaseObject, api){
 
+  // domain used to determine employee vs standard user
   var employeeDomain = 'pwc.com';
 
-  var profileCheck = function profileCheck(firebaseUserObject){
-    var ref = api.getRef('users', firebaseUserObject.uid);
+  /**
+   * Checks for the existance of user profile in the database
+   * @param  {Object} authObject - firebase authentication object
+   * @return {Promise} - resolves with profile FirebaseObject
+   */
+  var profileCheck = function profileCheck(authObject){
+    var ref = api.getRef('users', authObject.uid);
     var profile = $firebaseObject(ref);
 
     return profile.$loaded(function(){
       if(profile.$value === null){
-        return createProfile(profile, firebaseUserObject);
+        return createProfile(profile, authObject);
       } else {
         return profile;
       }
@@ -18,17 +24,29 @@ var settingsFactory = function settingsFactory($rootScope, $state, $log, $fireba
 
   };
 
-  var createProfile = function createProfile(fbo, authInfo){
-    fbo.displayName = authInfo.displayName;
-    fbo.email = authInfo.email;
-    fbo.photoURL = authInfo.photoURL;
-    fbo.employee = employeeCheck(authInfo.email);
+  /**
+   * Creates the sinder profile object based on mappings
+   * from the user's authentication info
+   * @param  {FirebaseObject} profile - firebase profile object
+   * @param  {Object} authObject - firebase authentication object
+   * @return {Promise} - resolves with profile FirebaseObject
+   */
+  var createProfile = function createProfile(profile, authObject){
+    profile.displayName = authObject.displayName;
+    profile.email = authObject.email;
+    profile.photoURL = authObject.photoURL;
+    profile.employee = employeeCheck(authObject.email);
 
-    return fbo.$save().then(function(){
-      return fbo;
+    return profile.$save().then(function(){
+      return profile;
     });
   };
 
+  /**
+   * Checks if email belongs to the org
+   * @param  {String} email - email address from the auth provider
+   * @return {Boolean}       
+   */
   function employeeCheck(email){
     return email.slice(-employeeDomain.length) === employeeDomain;
   }
