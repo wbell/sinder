fa5fa38e601955c6f45a0fdc7cc3e63b379fed38
Willie Bell <willie.bell@pwc.com>
2016-10-10 10:47:56 -0400

    add employee flag to profile creation

--- src/components/settings/factories/settings.factory.js
+++ src/components/settings/factories/settings.factory.js
@@ -2,6 +2,8 @@
 
 var settingsFactory = function settingsFactory($rootScope, $state, $log, $firebaseObject, api){
 
+  var employeeDomain = 'pwc.com';
+
   var profileCheck = function profileCheck(firebaseUserObject){
     var ref = api.getRef('users', firebaseUserObject.uid);
     var profile = $firebaseObject(ref);
@@ -20,11 +22,16 @@ var settingsFactory = function settingsFactory($rootScope, $state, $log, $fireba
     fbo.displayName = authInfo.displayName;
     fbo.email = authInfo.email;
     fbo.photoURL = authInfo.photoURL;
+    fbo.employee = employeeCheck(authInfo.email);
 
     return fbo.$save().then(function(){
       return fbo;
     });
   };
+
+  function employeeCheck(email){
+    return email.slice(-employeeDomain.length) === employeeDomain;
+  }
 
   return {
     profileCheck: profileCheck,
